2702be52d205273bc05785a007e5632575e14810
misaraadel <41232116+misaraadel@users.noreply.github.com>
2022-09-24 19:33:01 +0200

    edit owl carousel and arrow in best seller section

--- assets/js/lib/main.js
+++ assets/js/lib/main.js
@@ -1,3 +1,25 @@
+
+
+$(document).ready(function () {
+  $('.owl-best-seller').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1050: {
+        items: 4
+      }
+    }
+  })
+});
+
 var menu_btn = document.querySelector("#menu-btn");
 var sidebar = document.querySelector("#sidebar");
 var container = document.querySelector(".my-container");
