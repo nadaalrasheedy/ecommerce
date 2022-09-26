3ed1fe9442b9cdf054a170bb2e8492457d22717f
nadaalrasheedy <nadaalrasheedy04@gmail.com>
2022-09-27 00:59:29 +0200

    add responsive

--- assets/js/lib/main.js
+++ assets/js/lib/main.js
@@ -19,6 +19,25 @@ $(document).ready(function () {
     }
   })
 });
+$(document).ready(function () {
+  $('.owl-feedback').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1050: {
+        items: 3
+      }
+    }
+  })
+});
 
 var menu_btn = document.querySelector("#menu-btn");
 var sidebar = document.querySelector("#sidebar");
@@ -27,3 +46,14 @@ menu_btn.addEventListener("click", () => {
   sidebar.classList.toggle("active-nav");
   container.classList.toggle("active-cont");
 });
+
+const totop =document.querySelector(".to-top");
+
+window.addEventListener("scroll" ,() =>{
+  if(window.pageYOffset > 100) {
+    totop.classList.add("active");
+  }
+  else{
+    totop.classList.remove("active");x
+  }
+})
